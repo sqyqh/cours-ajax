a35c0365d51dfe5a63ae628631e2daab6cb804aa
sqyqh <sayah@elyat.im>
2021-02-24 12:02:03 +0100

    Explanations (first example)

--- examples/script.js
+++ examples/script.js
@@ -1,3 +1,5 @@
+// Permet de charger des données externes de manière asynchrone (au moment du clic).
+
 $(document).ready(function(){
 	$("button").click(function(){
 		$("#myDiv").load("data.txt");
